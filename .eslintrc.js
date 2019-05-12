0f2ac137a274a1e74e83829092d40cea36004d4a
Matias <matias.barriosn@gmail.com>
2019-05-12 13:18:42 -0400

    feat(eslint): vue-html setted

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,27 @@
 module.exports = {
+  root: true,
   env: {
-    browser: true,
-    es6: true
+    // this section will be used to determine which APIs are available to us
+    // (i.e are we running in a browser environment or a node.js env)
+    node: true,
+    browser: true
   },
-  extends:[
+  parserOptions: {
+    parser: "babel-eslint",
+    // specifying a module sourcetype prevent eslint from marking import statements as errors
+    sourceType: "module"
+  },
+  extends: [
+    // use the recommended rule set for both plain javascript and vue
+    "eslint:recommended",
+    "plugin:vue/recommended",
     'plugin:vue/essential',
     '@vue/airbnb',
     'plugin:vue-types/strongly-recommended',
-  ], 
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    parser: "babel-eslint"
-  },
-  plugins: [
-    'vue',
   ],
   rules: {
-    "vue/component-name-in-template-casing": ["error", "PascalCase"],
+    // we should always disable console logs and debugging in production
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
+  }
 };
