45a63c2e2514c546751daa731d68894f53284454
Matias <matias.barriosn@gmail.com>
2019-05-25 19:19:13 -0400

    chore: truffle-config.js created

---
+++ truffle-config.js
@@ -0,0 +1,25 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+require('dotenv').config()
+
+module.exports = {
+    networks: {
+        development: {
+            host: "127.0.0.1",
+            port: 8545, // Using ganache as development network
+            network_id: "*",
+        },
+        rinkeby: {
+            provider: function() {
+              return new HDWalletProvider(process.env.MNEMONIC, process.env.INFURA_PROVIDER)
+            },
+            gas: 6900000,
+            network_id: "4",
+        }
+    },
+    solc: {
+        optimizer: {
+            enabled: true,
+            runs: 200
+        }
+    }
+  };
