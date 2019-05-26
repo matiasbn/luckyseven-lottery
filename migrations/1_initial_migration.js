25597c114261d5944585b62fcdc704904c62d5ca
Matias <matias.barriosn@gmail.com>
2019-05-26 01:14:19 -0400

    chore: before changes on migration scripts

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable func-names */
 const Migrations = artifacts.require('../contracts/Migrations.sol');
 
 module.exports = function (deployer) {
--- migrations/2_deploy_Lucky7.js
+++ migrations/2_deploy_Lucky7.js
@@ -1,3 +1,7 @@
+/* eslint-disable max-len */
+/* eslint-disable func-names */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 const Lucky7Admin = artifacts.require('Lucky7Admin');
 const Lucky7TicketFactory = artifacts.require('Lucky7TicketFactory');
 const Lucky7Storage = artifacts.require('Lucky7Storage');
@@ -13,14 +17,12 @@ module.exports = function (deployer) {
   deployer.deploy(Lucky7Library);
   deployer.link(Lucky7Library, Lucky7TicketFactory);
   deployer.link(Lucky7Library, Lucky7Store);
-  deployer.deploy(Lucky7Storage).then(() => deployer.deploy(Lucky7Admin)).then(() => deployer.deploy(Lucky7TicketFactory, Lucky7Admin.address, Lucky7Storage.address, {
-    value: 7 * 2 * 300000 * 4000000000,
-  })).then(() => deployer.deploy(Lucky7Store, Lucky7Admin.address, Lucky7Storage.address, Lucky7TicketFactory.address, {
-    value: web3.toWei(1, 'ether'),
-  }));
+  deployer.deploy(Lucky7Storage)
+    .then(() => deployer.deploy(Lucky7Admin))
+    .then(() => deployer.deploy(Lucky7TicketFactory, Lucky7Admin.address, Lucky7Storage.address, {
+      value: web3.toWei(1, 'ether'),
+    }))
+    .then(() => deployer.deploy(Lucky7Store, Lucky7Admin.address, Lucky7Storage.address, Lucky7TicketFactory.address, {
+      value: web3.toWei(1, 'ether'),
+    }));
 };
-// deployer.deploy(Lucky7Admin);
-//
-
-// deployer.deploy(ArrayTes).then(function() {
-//   return deployer.deploy(CallFunction, ArrayTes.address);})
