e65537279c4f2bf59f7a20842d7cf433bddf4f0e
Matias <matias.barriosn@gmail.com>
2019-07-16 17:03:36 -0400

    chore: deployer updated to difference on rinkeby or localnetwork

--- migrations/3_deploy_Lucky7.js
+++ migrations/3_deploy_Lucky7.js
@@ -2,10 +2,28 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable func-names */
 const Lucky7Store = artifacts.require('Lucky7Store');
+const Lucky7Library = artifacts.require('Lucky7Library');
+const Lighthouse = artifacts.require('Lighthouse');
 
+module.exports = function (deployer, network) {
+  deployer.deploy(Lucky7Library);
+  deployer.link(Lucky7Library, Lucky7Store);
 
-module.exports = function (deployer) {
-  deployer.deploy(Lucky7Store, {
-    value: web3.utils.toWei('1', 'ether'),
-  });
+  if (network === 'rinkeby') {
+
+    var lighthouseAddress = '0x613D2159db9ca2fBB15670286900aD6c1C79cC9a';
+
+    deployer.deploy(Lucky7Store, lighthouseAddress, false, {
+      value: web3.utils.toWei('1', 'ether'),
+    });
+    
+  } else {
+
+    deployer.deploy(Lighthouse).then(function () {
+      return deployer.deploy(Lucky7Store, Lighthouse.address, true, {
+        value: web3.utils.toWei('1', 'ether'),
+      });
+    })
+
+  }
 };
